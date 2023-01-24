9c25f112709e7797846b4100b39e267e01f383a0
Mahmoud Hamzah <aboouhamzah@gmail.com>
2023-01-24 21:34:11 +0000

    Day 2 completed

---
+++ day_2/variables.py
@@ -0,0 +1,74 @@
+#Day 2 of python programming
+
+#Level_1
+#Question 1-13
+first_name="Mahmud"
+last_name="Hamzah"
+
+full_name="Mahmud Hamzah"
+country="Nigeria"
+
+city="kano"
+age="22"
+year="1999"
+is_married=False
+is_true=1
+is_light_on=True
+
+x,y,z=2,3,4
+
+#Level_2
+#Question 1
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Question 2
+print(len(first_name))
+
+#Question 3
+print(len(first_name)>len(last_name))
+
+
+#Question 4
+num_one=5
+num_two=4
+
+total=num_one + num_two
+variable_diff=num_one-num_two
+variable_product=num_one*num_two
+variable_division=num_one/num_two
+variable_remainder=num_two%num_one
+variable_exp=num_one**num_two
+floor_division=num_one//num_two
+
+#Question 5
+
+area_of_circle=30*30*3.142
+circum_of_circle=2*30*3.142
+
+radius=int(input("Enter radius :"))
+area_of_circle=radius*radius*3.142
+
+#Question 6
+
+first_name=input("Enter first name please ")
+last_name=input("Enter last name please ")
+country=input("Enter country please ")
+age=int(input("Enter age please "))
+
+#question 7
+help(print)
+
+
